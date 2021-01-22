37adfecbd6205b80f4b007d7089ada4c3f797ef1
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-22 20:28:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,13 @@
- 
+require 'pry' 
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object ({}) do |(key, value), final_array| 
+  data.each_with_object ({}) do |(key, value), final_array|
+    binding.pry 
     value.each do |key2, names|
       names.each do |name|
         if !final_array[name] 
           final_array[name] = {}
+          binding.pry
         end
         if !final_array[name][key]
           final_array[name][key] = []
